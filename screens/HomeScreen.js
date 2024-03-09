917d7cfa5e697c9d559306af8d45b77e4818651d
HuynhKhoaIT <>
2024-03-09 08:55:57 +0700

    call api

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,32 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect} from 'react'
 import Header from '../components/home/Header'
 import Stories from '../components/home/Stories'
 import Post from '../components/home/Post';
 import { POSTS } from '../data/posts';
 import BottomTab, { bottomTabIcons } from '../components/home/BottomTab';
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [ posts, setPosts ] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8082/api/posts");
+        setPosts(response.data);
+      } catch (error) {
+        console.log("error message", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <SafeAreaView style={{ backgroundColor: "#FFFFFF" }}>
       <Header/>
       <Stories/>
       <ScrollView style={{ height: '74%' }}>
-        {POSTS.map((post, index) => (
+        {posts.map((post, index) => (
           <Post post={post} key={index}/>
         ))}
       </ScrollView>
